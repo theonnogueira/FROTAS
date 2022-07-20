04e4e9e0a3a40a464a28d00dc434691889a1b5b5
theonnogueira <101525395+theonnogueira@users.noreply.github.com>
2022-07-20 16:42:48 -0300

    Delete Downloads directory

--- Downloads/projetoIntegrador-css/src/app/model/temas.ts
+++
@@ -1,7 +0,0 @@
-import { noticias } from "./noticias"
-
-export class temas {
-public id: number
-public descricao: string
-public noticias: noticias[]
-}
--- Downloads/projetoIntegrador-main/src/app/model/userLogin.ts
+++
@@ -1,9 +0,0 @@
-export class userLogin{
-    public id: number
-    public nome: string
-    public tipo: string
-    public email: string
-    public senha: string
-    public foto: string 
-    public token: string
-}
